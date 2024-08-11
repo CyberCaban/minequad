28e9397060ac3aa776c57c48ce3769ad1aed5730
CyberCaban <andrewgamer63@gmail.com>
2024-08-11 15:14:40 +0300

    Basic ui and block culling
    - added basic ui from macroquad
    - added partial render for block
    - fixed bug when camera jumps after ungrab
    - fixed uv on some polys

---
+++ src/systems/render_utils.rs
@@ -0,0 +1,136 @@
+use crate::systems::chunks::BlockMesh;
+use macroquad::prelude::*;
+
+use super::chunks::vert;
+
+pub fn mesh_top(mesh: &mut BlockMesh, pos: Vec3, texture: Texture2D) {
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 1.0, 0.0), vec2(1.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 0.0), vec2(0.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 1.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 1.0, 0.0), vec2(1.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 1.0, 1.0), vec2(1.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 1.0), vec2(0.0, 0.0)));
+
+    for _ in 0..6 {
+        mesh.indices.push(mesh.idx_counter as u16);
+        mesh.idx_counter += 1;
+    }
+
+    mesh.texture = Some(texture);
+}
+
+pub fn mesh_bottom(mesh: &mut BlockMesh, pos: Vec3, texture: Texture2D) {
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 0.0), vec2(1.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 0.0, 0.0), vec2(0.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 0.0, 1.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 0.0), vec2(1.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 1.0), vec2(1.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 0.0, 1.0), vec2(0.0, 0.0)));
+
+    for _ in 0..6 {
+        mesh.indices.push(mesh.idx_counter as u16);
+        mesh.idx_counter += 1;
+    }
+
+    mesh.texture = Some(texture);
+}
+
+pub fn mesh_front(mesh: &mut BlockMesh, pos: Vec3, texture: Texture2D) {
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 0.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 0.0, 0.0), vec2(1.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 0.0), vec2(1.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 0.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 1.0, 0.0), vec2(0.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 0.0), vec2(1.0, 1.0)));
+
+    for _ in 0..6 {
+        mesh.indices.push(mesh.idx_counter as u16);
+        mesh.idx_counter += 1;
+    }
+
+    mesh.texture = Some(texture);
+}
+
+pub fn mesh_back(mesh: &mut BlockMesh, pos: Vec3, texture: Texture2D) {
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 1.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 0.0, 1.0), vec2(1.0, 0.0)));
+    mesh.vertices   
+        .push(vert(pos + vec3(1.0, 1.0, 1.0), vec2(1.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 1.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 1.0, 1.0), vec2(0.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 1.0), vec2(1.0, 1.0)));
+
+    for _ in 0..6 {
+        mesh.indices.push(mesh.idx_counter as u16);
+        mesh.idx_counter += 1;
+    }
+
+    mesh.texture = Some(texture);
+} 
+
+pub fn mesh_left(mesh: &mut BlockMesh, pos: Vec3, texture: Texture2D) {
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 0.0, 0.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 0.0, 1.0), vec2(0.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 1.0), vec2(1.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 0.0, 0.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 0.0), vec2(1.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(1.0, 1.0, 1.0), vec2(1.0, 1.0)));
+
+    for _ in 0..6 {
+        mesh.indices.push(mesh.idx_counter as u16);
+        mesh.idx_counter += 1;
+    }
+
+    mesh.texture = Some(texture);
+}
+
+pub fn mesh_right(mesh: &mut BlockMesh, pos: Vec3, texture: Texture2D) {
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 1.0), vec2(0.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 0.0), vec2(0.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 1.0, 0.0), vec2(1.0, 0.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 0.0, 1.0), vec2(0.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 1.0, 1.0), vec2(1.0, 1.0)));
+    mesh.vertices
+        .push(vert(pos + vec3(0.0, 1.0, 0.0), vec2(1.0, 0.0)));
+
+    for _ in 0..6 {
+        mesh.indices.push(mesh.idx_counter as u16);
+        mesh.idx_counter += 1;
+    }
+
+    mesh.texture = Some(texture);
+}
